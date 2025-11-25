5284c8acfea1853962b0496b0566ffbb77d85d24
maryam-tariqq <162907496+maryam-tariqq@users.noreply.github.com>
2025-11-25 11:30:44 +0500

    Update sampleDataset to handle 100k entries

--- src/preprocessing/sampleDataset.py
+++ src/preprocessing/sampleDataset.py
@@ -1,10 +1,10 @@
 import json
 
 INPUT_FILE = "../../data/raw/arxiv.json"
-OUTPUT_FILE = "../../data/raw/arxiv_5.json"
+OUTPUT_FILE = "../../data/raw/arxiv_100k.json"
 
 
-def make_sample(n=5):
+def make_sample(n=100000):
     output = []
     count = 0
     skipped = 0
