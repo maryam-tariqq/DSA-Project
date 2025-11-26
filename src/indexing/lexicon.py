ae06f12a091894e411d87166505e9860a3331a14
khaliquemahnoor7-ui <khaliquemahnoor7@gmail.com>
2025-11-26 14:51:37 +0500

    Update lexicon.py

--- src/indexing/lexicon.py
+++ src/indexing/lexicon.py
@@ -4,18 +4,18 @@ import os
 INPUT_FILE = "../../data/processed/preprocessing.json"
 LEXICON_FILE = "../../data/processed/lexicon.json"
 
-# Step 1: Load existing lexicon (if exists)
+# Step 1: Load existing lexicon if it exists
 if os.path.exists(LEXICON_FILE):
     with open(LEXICON_FILE, "r", encoding="utf-8") as f:
         lexicon = json.load(f)
-    # Ensure keys are str and values are int
+    # Ensure keys are string and values are integers
     lexicon = {str(k): int(v) for k, v in lexicon.items()}
     current_id = max(lexicon.values()) + 1
 else:
     lexicon = {}
     current_id = 1
 
-# Step 2: Add new tokens only
+# Step 2: Adding new tokens only
 with open(INPUT_FILE, "r", encoding="utf-8") as f:
     docs = json.load(f)
 
@@ -25,7 +25,7 @@ for doc in docs:
             lexicon[token] = current_id
             current_id += 1
 
-# Step 3: Save updated lexicon
+# Step 3: Saves updated lexicon
 os.makedirs(os.path.dirname(LEXICON_FILE), exist_ok=True)
 with open(LEXICON_FILE, "w", encoding="utf-8") as f:
     json.dump(lexicon, f, indent=2)
